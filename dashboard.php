fa745c076a074fbdcb9495d69182ab248b992cf5
Fathan Alfariel <fathan.alfariel.2305356@students.um.ac.id>
2024-05-01 00:30:32 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+include("functions.php");
+global $conn;
 
 if(isset($_SESSION['data']['role'])){
     if($_SESSION['data']['role'] === "user"){
@@ -93,28 +95,129 @@ else{
             <div class="mt-12">
                 <h4 class="text-2xl font-semibold">Your orders</h4>
 
-                <div class="flex flex-col justify-center items-center bg-[#eeeeee] rounded-xl py-9 mt-7">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
-                        </svg>
-                    </span>
+                <?php 
+                    $sql = "SELECT * FROM pembelian;";
+                    $query = mysqli_query($conn, $sql);
 
-                    <div class="text-base mt-2.5">No orders</div>
-                </div>
+                    $i = 1;
+
+                    if(mysqli_num_rows($query) === 0) {    
+                ?>
+                    <div class="flex flex-col justify-center items-center bg-[#f5f5f5] rounded-3xl pb-9 mt-7">
+                        <dotlottie-player src="https://lottie.host/af002951-412c-4819-9ae3-5230d9394ecb/ZpkOOvunyr.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
+
+
+                        <div class="text-xl font-medium">No orders</div>
+                    </div>
+                <?php
+                    } else {
+                ?> 
+                    <div class="relative overflow-x-auto mt-5 bg-[#f5f5f5] rounded-3xl py-3 px-5">
+                        <table class="table-auto w-full text-left">
+                            <thead class="text-xs text-gray-700 uppercase border-b">
+                                <tr>
+                                    <th scope="col" class="px-1 py-3">
+                                        #
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Product
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Recipient name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Delivery address
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Food ordering
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Status
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    while($data = mysqli_fetch_array($query)) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $i++ ?></td>
+                                        <td class="px-6 py-4">
+                                            <div class="flex -space-x-4 rtl:space-x-reverse">
+                                                <?php
+                                                    // Mengambil gambar dari barang yang dibeli
+                                                    $getBarangDibeli = "SELECT gambar FROM barang WHERE id IN (SELECT id_barang FROM barang_dibeli WHERE id_pembelian = '{$data['id']}');";
+                                                    $getBarangDibeliQuery = mysqli_query($conn, $getBarangDibeli);
+
+                                                    while($dataGambar = mysqli_fetch_assoc($getBarangDibeliQuery)) {
+                                                ?>
+                                                    <img class="w-10 h-10 border-2 border-white rounded-full object-cover" 
+                                                        src="<?php echo "./images/" . $dataGambar['gambar']; ?>" 
+                                                        alt="produk_dibeli">
+                                                <?php
+                                                    }
+                                                ?>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 font-medium">
+                                            <?php echo $data['nama']; ?>
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <div class="text-sm">Phone number: <?php echo $data['phone']; ?></div>
+                                            <div class="text-sm">Delivery address: <?php echo $data['alamat']; ?></div>
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <?php
+                                                if($data['pemesanan_makanan'] === "take away") {
+                                                    echo "<span class='bg-rose-100 text-rose-800 text-xs font-medium py-1 px-2 rounded-full capitalize'>" . $data['pemesanan_makanan'] . "</span>";
+                                                } else {
+                                                    echo "<span class='bg-blue-100 text-blue-800 text-xs font-medium py-1 px-2 rounded-full capitalize'>" . $data['pemesanan_makanan'] . "</span>";
+                                                }
+                                            ?>
+                                        </td>
+                                        <td class="px-5 py-4">
+                                            <div class="relative inline-block group">
+                                                <button class="inline-flex items-center gap-x-1 py-0.5 pl-0.5 pr-1.5 hover:bg-white rounded-full">
+                                                    <span class="<?php echo ($data['status'] === "in process") ? 'bg-rose-100 text-rose-800' : (($data['status'] === "to receive") ? 'bg-blue-100 text-blue-800' : 'bg-green-100 text-green-800'); ?> text-xs font-medium py-1 px-2 rounded-full capitalize">
+                                                        <?php echo $data['status']; ?>
+                                                    </span>
+                                                
+                                                    <div class="invisible group-hover:visible">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                                        </svg>
+                                                    </div>
+                                                </button>
+
+                                                <div class="hidden group-hover:block absolute top-auto right-0 pt-0.5 z-10">
+                                                    <div class="flex flex-col gap-y-0.5 w-28 bg-white rounded-lg p-0.5">
+                                                        <option class="cursor-pointer px-2 py-1 text-sm <?php echo ($data['status'] === "in process") ? 'bg-[#eeeeee]' : 'hover:bg-[#eeeeee]'; ?> rounded-md" value="in process" selected>In process</option>
+                                                        <option class="cursor-pointer px-2 py-1 text-sm <?php echo ($data['status'] === "to receive") ? 'bg-[#eeeeee]' : 'hover:bg-[#eeeeee]'; ?> hover:bg-[#eeeeee] rounded-md" value="to receive">To receive</option>
+                                                        <option class="cursor-pointer px-2 py-1 text-sm <?php echo ($data['status'] === "completed") ? 'bg-[#eeeeee]' : 'hover:bg-[#eeeeee]'; ?> hover:bg-[#eeeeee] rounded-md" value="completed">Completed</option>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php } ?>
+                
             </div>
         </main>
 
         <script>
             // Dropdown Functions
             const menuDropdown = () => {
-            const menuElement = document.querySelector("#menu-dropdown");
+                const menuElement = document.querySelector("#menu-dropdown");
 
-            // Periksa apakah dropdown sudah terlihat
-            const isVisible = !menuElement.classList.contains("hidden");
+                // Periksa apakah dropdown sudah terlihat
+                const isVisible = !menuElement.classList.contains("hidden");
 
-            // Jika dropdown sudah terlihat, sembunyikan
-            if (isVisible) {
+                // Jika dropdown sudah terlihat, sembunyikan
+                if (isVisible) {
                     menuElement.classList.add("hidden");
                 } else {
                     // Jika dropdown belum terlihat, tampilkan
@@ -143,5 +246,8 @@ else{
             });
             // End Dropdown Functions
         </script>
+
+        <!-- Lottie Animations -->
+        <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
     </body>
 </html>
